ba14e57843b8f7ff2f3ab1138cfd5e5a2ab3f35b
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-21 02:04:03 +0900

    add neoforge

--- neoforge/src/main/java/net/pitan76/memoryusagetitle/neoforge/MemoryUsageTitleNeoForge.java
+++ neoforge/src/main/java/net/pitan76/memoryusagetitle/neoforge/MemoryUsageTitleNeoForge.java
@@ -1,11 +1,17 @@
 package net.pitan76.memoryusagetitle.neoforge;
 
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.pitan76.memoryusagetitle.MemoryUsageTitle;
 
 @Mod(MemoryUsageTitle.MOD_ID)
 public class MemoryUsageTitleNeoForge {
-    public MemoryUsageTitleNeoForge() {
+    public MemoryUsageTitleNeoForge(IEventBus modEventBus) {
+        modEventBus.addListener(this::onClientSetup);
+    }
+
+    public void onClientSetup(final FMLClientSetupEvent e) {
         MemoryUsageTitle.init();
     }
 }
