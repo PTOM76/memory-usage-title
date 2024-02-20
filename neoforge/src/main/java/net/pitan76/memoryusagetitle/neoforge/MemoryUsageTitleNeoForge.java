8475e60e414c70986e3ae819330aa3cccded5ca3
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-21 01:51:10 +0900

    add neoforge

---
+++ neoforge/src/main/java/net/pitan76/memoryusagetitle/neoforge/MemoryUsageTitleNeoForge.java
@@ -0,0 +1,11 @@
+package net.pitan76.memoryusagetitle.neoforge;
+
+import net.neoforged.fml.common.Mod;
+import net.pitan76.memoryusagetitle.MemoryUsageTitle;
+
+@Mod(SmallStairs.MOD_ID)
+public class MemoryUsageTitleNeoForge {
+    public MemoryUsageTitleNeoForge() {
+        MemoryUsageTitle.init();
+    }
+}
